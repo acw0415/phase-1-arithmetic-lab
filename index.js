fb02da9e53e7f65955c4e92a4c7185ad04106505
Austin <acw0415@gmail.com>
2022-06-29 12:07:10 -0400

    commit tree dc1dd2c2b040d4cb8cc21bf2b7daee74db6937fc

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let num3 = 27
+let num4 = 23
+
+const multiply = num1 * num2
+const random = Math.ceil(Math.random())
+const mod = num3 - num4
+const max = Math.max(1, 2, 3, 20)
 
 // Write your code here
